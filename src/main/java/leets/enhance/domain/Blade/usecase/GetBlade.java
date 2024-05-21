97f6d624b71f1b10639cf444d5b450e641a4f9cb
노정완 <jeongwan.noh@gmail.com>
2024-05-22 00:20:38 +0900

    feat: 아이템 조회 서비스 로직 구현

---
+++ src/main/java/leets/enhance/domain/Blade/usecase/GetBlade.java
@@ -0,0 +1,40 @@
+package leets.enhance.domain.Blade.usecase;
+
+import leets.enhance.domain.Blade.domain.Blade;
+import leets.enhance.domain.Blade.domain.repository.BladeRepository;
+import leets.enhance.domain.Blade.response.SingleItemResponse;
+import leets.enhance.domain.Blade.response.Top10ItemResponse;
+import leets.enhance.domain.user.domain.User;
+import leets.enhance.domain.user.domain.repository.UserRepository;
+import leets.enhance.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GetBlade {
+    private final BladeRepository bladeRepository;
+    private final UserRepository userRepository;
+
+    public SingleItemResponse executeForSingleUser(Authentication authentication) {
+        User user = userRepository.findByUsername(authentication.getName())
+                .orElseThrow(UserNotFoundException::new);
+
+        Blade blade = user.getBlade();
+
+        return SingleItemResponse.of(blade);
+    }
+
+    public Top10ItemResponse executeForTop10() {
+        List<Blade> top10Blade = bladeRepository.findTop10ByOrderByLevelDesc();
+
+        List<SingleItemResponse> top10Items = top10Blade.stream()
+                .map(SingleItemResponse::of)
+                .toList();
+
+        return Top10ItemResponse.from(top10Items);
+    }
+}
