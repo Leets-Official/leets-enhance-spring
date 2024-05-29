408e0ff23554da0ca4674f5822b1b0cdf900620f
jiixon <jr0107380@gmail.com>
2024-05-29 15:45:50 +0900

    feat: 오류시스템 및 시큐리티 설정, JWT 관련 서비스 구현

--- src/main/java/leets/enhance/domain/user/presentation/dto/Response/DuplicationResponseDto.java
+++ src/main/java/leets/enhance/domain/user/presentation/dto/Response/DuplicationResponseDto.java
@@ -1,4 +1,4 @@
-package leets.attendance.domain.user.presentation.dto.Response;
+package leets.enhance.domain.user.presentation.dto.Response;
 
 public record DuplicationResponseDto(
         boolean isDuplicated
